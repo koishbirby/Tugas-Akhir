2ec727f976a313b24c2a59d5731ada40aed1c8ac
BIRBYBERB <birbyyberb@gmail.com>
2025-11-27 21:49:23 +0700

    123

--- src/components/common/RevealAnimation.jsx
+++ src/components/common/RevealAnimation.jsx
@@ -12,18 +12,8 @@ export default function RevealAnimation() {
   }, []);
 
   return (
-    <div
-      className={`
-        fixed inset-0 z-[9999] flex
-        transition-opacity duration-500
-        ${hide ? "opacity-0 pointer-events-none" : "opacity-100"}
-      `}
-    >
-      {/* Left Panel */}
-      <div className="w-1/2 h-full bg-white animate-reveal-left"></div>
-
-      {/* Right Panel */}
-      <div className="w-1/2 h-full bg-white animate-reveal-right"></div>
+    <div className={`fixed inset-0 z-[9999] transition-opacity duration-500 ${hide ? "opacity-0 pointer-events-none" : "opacity-100"}`} >
+        <div className="w-full h-full bg-white animate-slide-down"></div>
     </div>
   );
 }
